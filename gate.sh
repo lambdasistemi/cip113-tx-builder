a520400c3cc1ef791beaa512579635f643c0c02b
paolino <paolo.veronelli@gmail.com>
2026-07-05 07:10:49 +0100

    chore: add cardano-node to e2e gate
    Tasks: T000

--- gate.sh
+++ gate.sh
@@ -5,4 +5,4 @@ git diff --check
 nix develop --quiet -c fourmolu --mode check src exe e2e-test
 nix develop --quiet -c hlint src exe e2e-test
 nix build .#e2e-tests
-(cd e2e-test && ../result/bin/e2e-tests)
+nix shell .#cardano-node -c bash -c 'cd e2e-test && ../result/bin/e2e-tests'
